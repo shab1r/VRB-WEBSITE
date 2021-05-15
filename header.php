0718c9b1c19d3c4c9fffc8f14aa308bfc50fa5ac
shab1r <0985130@hr.nl>
2021-05-15 17:58:51 +0200

    Made a start for the website template.

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>test systeem</title>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css"> 
+    </head>
+    <body>
+        
+                <!-- <a href="index.php"><img class="logo" src="  ===hier moet photo.png====" alt="Blogs logo"></a> -->
+                
+            
+    <div class="container">
+        <nav>Navbar
+
+
+          <ul>
+              <li><a href="index.php">Home</a></li>
+          </ul>
+
+        </nav>
